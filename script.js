4d68f6c867da18ae0f48b3001cac4ec08c3be56b
Yanet Olmedo <yanetolmedo92@gmail.com>
2023-05-02 23:40:53 -0300

    Se agregó la nueva información y se ajustaron las funciones

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
-// Crear una lista de contactos con datos predefinidos
+// Lista de contactos con datos predefinidos
 let listaContactos = [
-  {nombre: "Homero Simpson", telefono: "1234-5678"},
-  {nombre: "Marge Simpson", telefono: "2345-678"},
-  {nombre: "Bart Simpson", telefono: "3456-7890"},
-  {nombre: "Maggie Simpson", telefono: "4567-8910"}
+  { id: 1, nombre: "homero", apellido: "simpson", telefono: "12345678", ubicacion: { ciudad: "springfield", direccion: "evergreen terrace 742" } },
+  { id: 2, nombre: "marge", apellido: "simpson", telefono: "12345678", ubicacion: { ciudad: "springfield", direccion: "evergreen terrace 742" } },
+  { id: 3, nombre: "bart", apellido: "simpson", telefono: "12345678", ubicacion: { ciudad: "springfield", direccion: "evergreen terrace 742" } },
+  { id: 4, nombre: "lisa", apellido: "simpson", telefono: "12345678", ubicacion: { ciudad: "springfield", direccion: "evergreen terrace 742" } },
+  { id: 5, nombre: "maggie", apellido: "simpson", telefono: "1234678", ubicacion: { ciudad: "springfield", direccion: "evergreen terrace 742" } }
 ];
 
-// Función para añadir un nuevo contacto a la lista. 
+// Añadir un nuevo contacto a la lista. 
 function agregarContacto() {
+  let nuevoId = 5;
   let continuar = true;
   while (continuar) {
-    let nom = prompt("Ingrese el nombre y apellido del contacto");
-    let tel = prompt("Ingrese el teléfono del contacto");
-    let nuevoContacto = {nombre: nom, telefono: tel};
+    let nom = prompt("Ingrese el nombre").toLowerCase(); //le pide al usuario que ingrese cada registro y pasamos todo a minuscula para igualar
+    let ap = prompt("Ingrese el apellido").toLowerCase();
+    let tel = prompt("Ingrese el teléfono del contacto sin guiones");
+    let ciu = prompt("Ingrese la ciudad del contacto").toLowerCase();
+    let direc = prompt("Ingrese la dirección").toLowerCase();
+
+    let nuevoContacto = { id: nuevoId + 1, nombre: nom, apellido: ap, telefono: tel, ubicacion: { ciudad: ciu, direccion: direc } }; //asignamos los registros a un contacto
     
     listaContactos.push(nuevoContacto);
+    nuevoId = nuevoId + 1;
     let r = prompt("Desea agregar otro contacto? (S/N)"); //si elije "S", se reinicia el ciclo
     if (r.toUpperCase() !== "S") { //si elije "N" continuar cambia a falso y sale del bucle.
       continuar = false;
     }
   }
-
 }
 
-
-// Función para borrar un contacto existente de la lista
+// Borrar un contacto existente de la lista
 function borrarContacto(nombre) {
-  for (let i = 0; i < listaContactos.length; i++) {
-    if (listaContactos[i].nombre === nombre) {
-      listaContactos.splice(i, 1);
-      break;
-    }
+  let indiceContacto = listaContactos.findIndex(contacto => { //busca el contacto
+    return (contacto.nombre + " " + contacto.apellido) === nombre;
+  });
+  if ((indiceContacto !== -1)) { //si lo encuentra, lo borra
+    console.log(nombre + " fue eliminado/a de la lista con éxito")
+    return listaContactos.splice(indiceContacto, 1);
   }
 }
 
-// Función para imprimir los contactos en la lista
+// Imprimir los contactos en la lista
 function imprimirContactos() {
-  console.log("Contact List:");
-  for (let i = 0; i < listaContactos.length; i++) {
-    console.log(listaContactos[i].nombre + " - " + listaContactos[i].telefono);
+  console.log("Lista de contactos:");
+  for (let contacto of listaContactos) {
+    console.log(contacto);
   }
 }
 
 // Testeo de funciones.
 agregarContacto()
-borrarContacto("Maggie Simpson");
+borrarContacto("maggie simpson");
 imprimirContactos();
