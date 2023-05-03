cd0ce692cca90f1d8b9edafd18e67d273315debb
Yanet Olmedo <yanetolmedo92@gmail.com>
2023-05-03 01:40:08 -0300

    Nueva funcion para actualizar contactos

--- script.js
+++ script.js
@@ -48,7 +48,56 @@ function imprimirContactos() {
   }
 }
 
+function actualizarContacto() {
+  let continuar = true;
+  while (continuar) {
+    let nombre = prompt("Ingrese el nombre y apellido del contacto que desea actualizar").toLowerCase();
+
+    // Buscar el contacto
+    let contacto = listaContactos.find((c) => {
+      return (c.nombre + " " + c.apellido).toLowerCase() === nombre;
+    });
+  
+    if (contacto) {
+      let campo = prompt("Ingrese el campo que desea actualizar (nombre, apellido, telefono, ubicacion)").toLowerCase();
+  
+      if (campo === "nombre" || campo === "apellido" || campo === "telefono") {
+        let nuevoValor = prompt("Ingrese el nuevo valor para el campo " + campo).toLowerCase();
+  
+        // Actualizar campo.
+        contacto[campo] = nuevoValor;
+  
+        console.log(`El contacto de ${nombre} fue actualizado con éxito`);
+      } else if (campo === "ubicacion") {
+        let subCampo = prompt("Ingrese el campo de ubicación que desea actualizar (ciudad, direccion)");
+  
+        if (subCampo === "ciudad" || subCampo === "direccion") {
+          let nuevoValor = prompt(`Ingrese el nuevo valor para el campo ${campo}: ${subCampo}`).toLowerCase();
+  
+          // Actualizar un subcampo dentro de la ubicacion
+          contacto.ubicacion[subCampo] = nuevoValor;
+  
+          console.log(`El contacto de ${nombre} fue actualizado con éxito`);
+        } else {
+          console.log("Campo inválido"); //Validacion para campos inexistentes o incorrectos
+        }
+      } else {
+        console.log("Campo inválido");
+      }
+    } else {
+      console.log("No se encontró ningún contacto con ese nombre y apellido"); //validacion para conctactos inexistentes o incorrectos.
+    }
+    let r = prompt("Desea actualizar otro contacto? (S/N)"); //si elije "S", se reinicia el ciclo
+    if (r.toUpperCase() !== "S") { //si elije "N", "continuar" cambia a falso y sale del bucle
+      continuar = false;
+    }
+
+  }
+
+}
+
 // Testeo de funciones.
 agregarContacto()
 borrarContacto("maggie simpson");
+actualizarContacto();
 imprimirContactos();
